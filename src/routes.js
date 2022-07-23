c7da2b933b1a6caba7e1fa15876027b599a228d1
hee000 <79568286+hee000@users.noreply.github.com>
2022-07-23 23:16:54 +0900

    딥러닝 추가

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Optimizers from './pages/ModelSelection/Optimizers';
 import Losses from './pages/ModelSelection/Losses';
 import { useRoutes } from 'react-router-dom';
 import MakeLayers from './pages/ModelCreate/MakeLayers';
+import TfTest from './pages/TfTest';
 
 function Routes() {
   let element = useRoutes([
@@ -14,7 +15,8 @@ function Routes() {
     { path: '/gradients/', element: <Gradients /> },
     { path: '/optimizers/', element: <Optimizers /> },
     { path: '/losses/', element: <Losses /> },
-    { path: '/layers/', element: <MakeLayers /> }
+    { path: '/layers/', element: <MakeLayers /> },
+    { path: '/tfjs_test/', element: <TfTest /> }
   ]);
   return element;
 }
