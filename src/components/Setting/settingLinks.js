291a736c1544930af7a97ee50e4476c956dd60b1
mooooongni <mooooongni@gmail.com>
2022-12-11 16:40:30 +0900

    빌드 업로드

--- src/components/Setting/settingLinks.js
+++
@@ -1,5 +0,0 @@
-export const settingLinks = [
-	{ navLinkId: 'Layer', scrollToId: 'layerContainer' },
-	{ navLinkId: 'Compile', scrollToId: 'compileContainer' },
-	{ navLinkId: 'Param', scrollToId: 'paramContainer' },
-];
